79c6fb46a7089ffa86ecf2f88f7a2dc27c19490d
Zhenya <e.kalenkovich@gmail.com>
2022-01-17 19:23:18 -0500

    refactor: more pure functions
    Modifying lists defined outside the function is really hard to track (for me, at least).

--- listen_time.py
+++ listen_time.py
@@ -272,56 +272,48 @@ def count_annotations_within_subregion(region_map, clan_file):
     return counts
 
 
+def get_total_time_and_count(region_map, region_type):
+    assert region_type in REGION_TYPES
+    start_times = region_map[region_type]['starts']
+    end_times = region_map[region_type]['ends']
+    total_time = 0
+    for i in range(len(start_times)):
+        total_time += end_times[i] - start_times[i]
+    return total_time, len(start_times)
+
+
+def skip_silence_overlap_time(region_map):
+    """ This is only used for month 6 and 7.
+        The total time where skip and silence regions overlap are computed so as to be subtracted from silence time
+        computed later.
+    """
+    skip_start_times = region_map['skip']['starts']
+    skip_end_times = region_map['skip']['ends']
+    silence_start_times = region_map['silence']['starts']
+    silence_end_times = region_map['silence']['ends']
+    overlap_time = 0
+    for i in range(len(skip_start_times)):
+        for j in range(len(silence_start_times)):
+            if skip_start_times[i]>=silence_start_times[j] and skip_start_times[i]<=silence_end_times[j]:
+                overlap_time += min(silence_end_times[j], skip_end_times[i]) - skip_start_times[i]
+            elif skip_end_times[i]>=silence_start_times[j] and skip_end_times[i]<=silence_end_times[j]:
+                overlap_time += skip_end_times[i] - max(silence_start_times[j], skip_start_times[i])
+    return overlap_time
+
+
 def total_listen_time(clan_file: pyclan.ClanFile, region_map, month67=False):
     """
     Step 4:
         Compute the total listen time. Several transformations or filterings are done before computing the total listen
         time.
     """
-
-    def skip_silence_overlap_time():
-        """ This is only used for month 6 and 7.
-            The total time where skip and silence regions overlap are computed so as to be subtracted from silence time
-            computed later.
-        """
-        skip_start_times = region_map['skip']['starts']
-        skip_end_times = region_map['skip']['ends']
-        silence_start_times = region_map['silence']['starts']
-        silence_end_times = region_map['silence']['ends']
-        overlap_time = 0
-        for i in range(len(skip_start_times)):
-            for j in range(len(silence_start_times)):
-                if skip_start_times[i]>=silence_start_times[j] and skip_start_times[i]<=silence_end_times[j]:
-                    overlap_time += min(silence_end_times[j], skip_end_times[i]) - skip_start_times[i]
-                elif skip_end_times[i]>=silence_start_times[j] and skip_end_times[i]<=silence_end_times[j]:
-                    overlap_time += skip_end_times[i] - max(silence_start_times[j], skip_start_times[i])
-        return overlap_time
-
-    def _region_total_time_and_count(region_type):
-        assert region_type in REGION_TYPES
-        start_times = region_map[region_type]['starts']
-        end_times = region_map[region_type]['ends']
-        total_time = 0
-        for i in range(len(start_times)):
-            total_time += end_times[i] - start_times[i]
-        return total_time, len(start_times)
-
-    # I have those functions all separated in case we need to make modifications to the way we compute listen time for
-    # each region
-    annotated_subregion_time = partial(_region_total_time_and_count, 'subregion')
-    skip_region_time = partial(_region_total_time_and_count, 'skip')
-    silence_region_time = partial(_region_total_time_and_count, 'silence')
-    extra_region_time = partial(_region_total_time_and_count, 'extra')
-    makeup_region_time = partial(_region_total_time_and_count, 'makeup')
-    surplus_region_time = partial(_region_total_time_and_count, 'surplus')
-
     result = {}
 
     # Here we add the raw totals for skip and subregion to the result dictionary. By raw, we mean that the preprocessing
     # steps below are not done. These items are for diagnostic purposes.
 
-    result['silence_raw_hour'] = ms2hr(silence_region_time()[0])
-    shour, snum = annotated_subregion_time()
+    result['silence_raw_hour'] = ms2hr(get_total_time_and_count(region_map, 'silence')[0])
+    shour, snum = get_total_time_and_count(region_map, 'subregion')
     result['num_raw_subregion'], result['subregion_raw_hour'] = snum, ms2hr(shour)
 
     result['annotation_counts_raw'] = count_annotations_within_subregion(region_map=region_map, clan_file=clan_file)
@@ -331,28 +323,28 @@ def total_listen_time(clan_file: pyclan.ClanFile, region_map, month67=False):
     else:
         removals = [''] * 5
 
-    skip_silence_time = skip_silence_overlap_time()
+    skip_silence_time = skip_silence_overlap_time(region_map)
     result['skip_silence_overlap_hour'] = ms2hr(skip_silence_time)
         
-    subregion_time, num_subregion_with_annot = annotated_subregion_time()
+    subregion_time, num_subregion_with_annot = get_total_time_and_count(region_map, 'subregion')
     result['subregion_time'] = subregion_time
     result['num_subregion_with_annot'] = num_subregion_with_annot
 
-    skip_time, _ = skip_region_time()
+    skip_time, _ = get_total_time_and_count(region_map, 'skip')
     result['skip_time'] = skip_time
 
-    silence_time, _ = silence_region_time()
+    silence_time, _ = get_total_time_and_count(region_map, 'silence')
     result['silence_time'] = silence_time
 
-    extra_time, num_extra_region = extra_region_time()
+    extra_time, num_extra_region = get_total_time_and_count(region_map, 'extra')
     result['extra_time'] = extra_time
     result['num_extra_region'] = num_extra_region
 
-    makeup_time, num_makeup_region = makeup_region_time()
+    makeup_time, num_makeup_region = get_total_time_and_count(region_map, 'makeup')
     result['makeup_time'] = makeup_time
     result['num_makeup_region'] = num_makeup_region
 
-    surplus_time, num_surplus_region = surplus_region_time()
+    surplus_time, num_surplus_region = get_total_time_and_count(region_map, 'surplus')
     result['surplus_time'] = surplus_time
     result['num_surplus_region'] = num_surplus_region
 
@@ -368,7 +360,8 @@ def total_listen_time(clan_file: pyclan.ClanFile, region_map, month67=False):
     # Otherwise, we assume that the entire file was listened to, so we do not touch anything. 
     else:
         total_time = clan_file.line_map[-1].offset - (skip_time + silence_time - skip_silence_time)
-        print('{} - ({} + {} - {}) == {}'.format(clan_file.line_map[-1].offset, skip_time, silence_time, skip_silence_time, total_time))
+        print('{} - ({} + {} - {}) == {}'.format(clan_file.line_map[-1].offset, skip_time, silence_time,
+                                                 skip_silence_time, total_time))
         print(clan_file.line_map[-1].offset - (skip_time + silence_time - skip_silence_time) == total_time)
         
     result['total_listen_time'] = total_time
